f3de36572162f4e26d3b8a96a395d4e28428dfb0
fis <ybbs.daans@hotmail.com>
2018-04-06 06:32:28 +0800

    Building arrayfire without git.
    * arrayfire.scm (forge): New public variable.
    (make-arrayfire): Build graphic.

--- arrayfire.scm
+++ arrayfire.scm
@@ -39,11 +39,71 @@
   #:use-module (gnu packages python)
   #:use-module (gnu packages version-control))
 
-(define* (make-arrayfire cl? cuda? graphics? name #:optional (cpu? #t))
+(define-public glbinding
+  (package
+   (name "glbinding")
+   (version "2.1.4")
+   (source (origin
+	    (method url-fetch)
+	    (uri (string-append "https://github.com/cginternals/"
+				name "/archive/v" version ".tar.gz"))
+	    (sha256
+	     (base32 "1qlbixc26z0dsj0c4h8dxq98qazx0f43ccad62r1gln0hsq72nfb"))))
+   (build-system cmake-build-system)
+   (inputs `(("glfw" ,glfw)
+	     ("python" ,python)))
+   (home-page "http://www.glbinding.org")
+   (synopsis "A C++ binding for the OpenGL API, generated using the gl.xml specification")
+   (description "Glbinding leverages modern C++11 features like enum classes, lambdas, and variadic templates, instead of relying on macros.")
+   (license license:non-copyleft)))
+
+
+(define-public forge
+  (package
+    (name "forge")
+    (version "1.0.2")
+    (source (origin
+	      (method url-fetch)
+	      (uri (string-append
+		    "https://github.com/arrayfire/forge/archive/v"
+		    version "-ft.tar.gz"))
+	      (sha256
+	       (base32 "0m0bc75a6gdq84gs3sh069f7q0idmgh1ikc6znjjm2hxc35pz7wd"))))
+    (native-inputs `(("cl2hpp-header" ,cl2hpp-header)
+		     ("glm" ,glm)))
+    (inputs `(("freeimage" ,freeimage)
+	      ("freetype" ,freetype)
+	      ("fontconfig" ,fontconfig)
+	      ("glfw" ,glfw)
+	      ("glu" ,glu) 
+	      ("boost" ,boost)
+	      ("glbinding" ,glbinding)))
+    (build-system cmake-build-system)
+    (arguments
+     `(#:configure-flags '("-DUSE_SYSTEM_CL2HPP=ON"
+			   "-DUSE_SYSTEM_GLBINDING=ON"
+			   "-DUSE_SYSTEM_FREETYPE=ON"
+			   "-DUSE_SYSTEM_GLM=ON"
+			   "-DBUILD_DOCUMENTATION=OFF"
+			   "-DBUILD_EXAMPLES=OFF")
+			 #:tests? #f))
+    (home-page "https://github.com/arrayfire/forge")
+    (synopsis "High Performance Visualization.")
+    (description "An OpenGL interop library that can be used with ArrayFire or
+any other application using CUDA or OpenCL compute backend. The goal of Forge
+is to provide high performance OpenGL visualizations for C/C++ applications
+that use CUDA/OpenCL. ")
+    (license license:bsd-3)))
+
+
+(define* (make-arrayfire cl? cuda? name #:optional (cpu? #t))
   (let* ((cpu-native `(,cpu? (("googletest" ,googletest)
 			      ("pkg-config" ,pkg-config))))
 	 (cpu-inputs `(,cpu? (("freeimage" ,freeimage)
 			      ("openblas" ,openblas)
+			      ("glbinding" ,glbinding)
+			      ("forge" ,forge)
+			      ("glew" ,glew)
 			      ("fftw" ,fftw)
 			      ("fftwf" ,fftwf)
 			      ("fftw-avx" ,fftw-avx)
@@ -62,7 +122,18 @@
 			(("cuda" ,cuda))))
 	 (native-inputs `(,cpu-native ,cl-native ,cuda-native))
 	 (inputs `(,cpu-inputs ,cl-inputs ,cuda-inputs))
-
+	 (switchs (lambda (s) (if s "ON" "OFF")))
+	 (flags `(list
+		  (string-append "-DBUILD_CUDA=" (if ,cuda? "ON" "OFF"))
+		  (string-append "-DBUILD_OPENCL=" (if ,cl? "ON" "OFF"))
+		  ;; (if ,cl? "-DUSE_SYSTEM_CLBLAS=ON" "-Wdev")
+		  ;; (if ,cl? "-DUSE_SYSTEM_CLFFT=ON" "-Wdev")
+		  ;; (if ,cl? "-DUSE_SYSTEM_CL2HPP=ON" "-Wdev")
+		  "-DBUILD_GRAPHICS=ON"
+		  "-DUSE_SYSTEM_FORGE=ON"
+		  "-DUSE_SYSTEM_GLBINDING=ON"
+		  "-DUSE_SYSTEM_BOOST_COMPUTE=ON"
+		  "-DBUILD_TEST=ON"))
 	 ;; (switchs (lambda (s) (if s "ON" "OFF")))
 	 )
     (define (make-inputs inputs-list)
@@ -73,64 +144,56 @@
 	    (if (equal? (car first) #t)
 		(append (cadr first) (make-inputs rest))
 		(make-inputs rest)))))
-    (define (switchs s)
-      (if s "ON" "OFF"))
-    (define (make-flags)
-      `((string-append "-DBUILD_CUDA=" ,(switchs cuda?))
-	(string-append "-DBUILD_OPENCL=" ,(switchs cl?))
-	(if ,cl? "-DUSE_SYSTEM_CLBLAS=ON" "")
-	(if ,cl? "-DUSE_SYSTEM_CLFFT=OFF" "")
-	(if ,cl? "-DUSE_SYSTEM_CL2HPP=ON" "")
-	(string-append "-DBUILD_GRAPHICS" ,(switchs graphics?))
-	"-DUSE_SYSTEM_FORGE=ON"
-	"-DUSE_SYSTEM_BOOST_COMPUTE=ON"
-	"-DBUILD_TEST=ON"))
+    ;; (define (switchs s)
+    ;;   (if s "ON" "OFF"))
+    ;; (define make-flags
+    ;;   `(,(string-append "-DBUILD_CUDA=" (switchs cuda?))
+    ;; 	,(string-append "-DBUILD_OPENCL=" (switchs cl?))
+    ;; 	,(if cl? "-DUSE_SYSTEM_CLBLAS=ON" "")
+    ;; 	,(if cl? "-DUSE_SYSTEM_CLFFT=OFF" "")
+    ;; 	,(if cl? "-DUSE_SYSTEM_CL2HPP=ON" "")
+    ;; 	"-DBUILD_GRAPHICS=ON"
+    ;; 	"-DUSE_SYSTEM_FORGE=ON"
+    ;; 	"-DUSE_SYSTEM_BOOST_COMPUTE=ON"
+    ;; 	"-DBUILD_TEST=ON"))
     (package
-     (name name)
-     (version "3.5.1")
-     (source (origin
-	      (method url-fetch)
-	      (uri (string-append
-		    "http://arrayfire.com/arrayfire_source/arrayfire-full-"
-		    version ".tar.bz2"))
-	      (file-name (string-append name "-full-" version ".tar.bz2"))
-	      ;; (patches (search-patches
-	      ;; 	      "Restore-USE_SYSTEM_CLBLAS-and-USE_SYSTEM_CLFFT.patch"))
-	      (sha256
-	       (base32
-		"1w11kfw20nqhvw8fnrab6n4cs8a7az3fq7xygrnq4kcx4zy2zzxn"))))
-     (native-inputs (make-inputs native-inputs))
-     (inputs (make-inputs inputs))
-     (build-system cmake-build-system)
-     (arguments
-      ;; For master branch, use: AF_BUILD_CL, AF_WITH_GRAPHICS
-      ;; USE_SYSTEM_* need to be patched back.
-      ;; Actually, arrayfire will disable the corresponding backends if
-      ;; dependency is missing.
-      `(#:configure-flags `(,(if ,cuda? "-DBUILD_CUDA=ON" "-DBUILD_CUDA=OFF")
-			    ,(if ,cl? "-DBUILD_CL=ON" "-DBUILD_CL=OFF")
-			    "-DUSE_SYSTEM_CLBLAS=ON"
-			    "-DUSE_SYSTEM_CLFFT=ON"
-			    "-DUSE_SYSTEM_CL2HPP=ON"
-			    "-DBUILD_GRAPHICS=ON"
-			    "-DUSE_SYSTEM_FORGE=ON"
-			    "-DUSE_SYSTEM_BOOST_COMPUTE=ON"
-			    "-DBUILD_TEST=ON")))
-     (home-page "http://arrayfire.com/")
-     (synopsis "ArrayFire: a general purpose GPU library.")
-     (description "ArrayFire is a general-purpose library that simplifies the
+      (name name)
+      (version "3.5.1")
+      (source (origin
+		(method url-fetch)
+		(uri (string-append
+		      "http://arrayfire.com/arrayfire_source/arrayfire-full-"
+		      version ".tar.bz2"))
+		(file-name (string-append name "-full-" version ".tar.bz2"))
+		;; (patches (search-patches
+		;; 	      "Restore-USE_SYSTEM_CLBLAS-and-USE_SYSTEM_CLFFT.patch"))
+		(sha256
+		 (base32
+		  "1w11kfw20nqhvw8fnrab6n4cs8a7az3fq7xygrnq4kcx4zy2zzxn"))))
+      (native-inputs (make-inputs native-inputs))
+      (inputs (make-inputs inputs))
+      (build-system cmake-build-system)
+      (arguments
+       ;; For master branch, use: AF_BUILD_CL, AF_WITH_GRAPHICS
+       ;; USE_SYSTEM_* need to be patched back.
+       ;; Actually, arrayfire will disable the corresponding backends if
+       ;; dependency is missing.
+       `(#:configure-flags ,flags))
+      (home-page "http://arrayfire.com/")
+      (synopsis "ArrayFire: a general purpose GPU library.")
+      (description "ArrayFire is a general-purpose library that simplifies the
 process of developing software that targets parallel and massively-parallel
 architectures including CPUs, GPUs, and other hardware acceleration devices.")
-     (license (list license:bsd-3)))))
+      (license (list license:bsd-3)))))
 
 (define-public arrayfire-cpu
-  (make-arrayfire #f #f #f "arrayfire-cpu"))
+  (make-arrayfire #f #f "arrayfire-cpu"))
 (define-public arrayfire-cl
-  (make-arrayfire #t #f #f "arrayfire-cl"))
+  (make-arrayfire #t #f "arrayfire-cl"))
 (define-public arrayfire-cuda
-  (make-arrayfire #f #t #f "arrayfire-cuda"))
+  (make-arrayfire #f #t "arrayfire-cuda"))
 (define-public arrayfire-full
-  (make-arrayfire #t #t #f "arrayfire-full"))
+  (make-arrayfire #t #t "arrayfire-full"))
 
 (define-public glm
   (package
@@ -156,59 +219,6 @@ library for graphics software based on the OpenGL Shading Language (GLSL)
 specifications.")
    (license license:expat)))
 
-(define-public forge
-  (package
-   (name "forge")
-   (version "1.0.2")
-   (source (origin
-	    (method url-fetch)
-	    (uri (string-append
-		  "https://github.com/arrayfire/forge/archive/v"
-		  version "-ft.tar.gz"))
-	    (sha256
-	     (base32 "0m0bc75a6gdq84gs3sh069f7q0idmgh1ikc6znjjm2hxc35pz7wd"))))
-   (native-inputs `(("cl2hpp-header" ,cl2hpp-header)
-		    ("glm" ,glm)))
-   (inputs `(("freeimage" ,freeimage)
-	     ("freetype" ,freetype)
-	     ("fontconfig" ,fontconfig)
-	     ("glfw" ,glfw)
-	     ("boost" ,boost)
-	     ("glbinding" ,glbinding)))
-   (build-system cmake-build-system)
-   (arguments
-    `(#:configure-flags '("-DUSE_SYSTEM_CL2HPP=ON"
-			  "-DUSE_SYSTEM_GLBINDING=ON"
-			  "-DUSE_SYSTEM_FREETYPE=ON"
-			  "-DUSE_SYSTEM_GLM=ON"
-			  "-DBUILD_DOCUMENTATION=OFF"
-			  "-DBUILD_EXAMPLES=OFF")
-      #:tests? #f))
-   (home-page "https://github.com/arrayfire/forge")
-   (synopsis "High Performance Visualization.")
-   (description "An OpenGL interop library that can be used with ArrayFire or
-any other application using CUDA or OpenCL compute backend. The goal of Forge
-is to provide high performance OpenGL visualizations for C/C++ applications
-that use CUDA/OpenCL. ")
-   (license license:bsd-3)))
-
-(define-public glbinding
-  (package
-   (name "glbinding")
-   (version "2.1.4")
-   (source (origin
-	    (method url-fetch)
-	    (uri (string-append "https://github.com/cginternals/"
-				name "/archive/v" version ".tar.gz"))
-	    (sha256
-	     (base32 "1qlbixc26z0dsj0c4h8dxq98qazx0f43ccad62r1gln0hsq72nfb"))))
-   (build-system cmake-build-system)
-   (inputs `(("glfw" ,glfw)
-	     ("python" ,python)))
-   (home-page "http://www.glbinding.org")
-   (synopsis "A C++ binding for the OpenGL API, generated using the gl.xml specification")
-   (description "Glbinding leverages modern C++11 features like enum classes, lambdas, and variadic templates, instead of relying on macros.")
-   (license license:non-copyleft)))
 
 ;; FIXME: Dead code, to be remove. 
 (define-public arrayfire
