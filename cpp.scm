042c87df619b36cd95f9634aa338d4a4c5d88e8b
fis <ybbs.daans@hotmail.com>
2018-05-07 22:34:08 +0800

    Add xtl.
    * cpp.scm (xtl): New variable.

--- cpp.scm
+++ cpp.scm
@@ -19,7 +19,8 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix download)
-  #:use-module (guix build-system cmake))
+  #:use-module (guix build-system cmake)
+  #:use-module (gnu packages check))
 
 (define-public cxx-json
   (package
@@ -38,4 +39,38 @@
     (build-system cmake-build-system)
     (synopsis "JSON for Modern C++")
     (description "JSON for Modern C++")
+    (license license:expat)))
+
+(define-public xtl
+  (package
+    (name "xtl")
+    (version "0.4.8")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://github.com/QuantStack/xtl/archive/"
+                    version ".tar.gz"))
+              (sha256
+               (base32
+                "05bcz9y590b77bxcip0k31rgsapmkwqi1smvsvc84zz7m87d4jvy"))
+              (file-name (string-append name "-" version ".tar.gz"))))
+    (native-inputs
+     `(("googletest" ,googletest)
+       ("cxx-json" ,cxx-json)))
+    (arguments
+     `(#:configure-flags
+       '("-DBUILD_TESTS=ON")
+       #:phases
+       (modify-phases %standard-phases
+         (replace 'check
+           (lambda* _
+             (invoke "make" "test_xtl")
+             (chdir "test")
+             (invoke "./test_xtl")
+             (chdir ".."))))))
+    (home-page "https://github.com/QuantStack/xtl")
+    (build-system cmake-build-system)
+    (synopsis "C++ template library providing some basic tools")
+    (description "xtl is a C++ header-only librar template providing basic
+tools (containers, algorithms) used by other quantstack packages")
     (license license:expat)))
