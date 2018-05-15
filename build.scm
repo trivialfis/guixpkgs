8dccffbac5ca3fbacae40214d600987a16e8f030
fis <ybbs.daans@hotmail.com>
2018-05-15 19:29:53 +0800

    Fix bear.
    * build.scm (bear): New variable.

--- build.scm
+++ build.scm
@@ -1,6 +1,9 @@
 (define-module (build)
   #:use-module (guix packages)
   #:use-module (guix git-download)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system cmake)
   #:use-module (guix build-system python)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages python)
@@ -34,3 +37,35 @@ Python.")
       (description "A straightforward but powerful build system based on Ninja
 and Python.")
       (license license:asl2.0))))
+
+(define-public bear
+  (package
+    (name "bear")
+    (version "2.3.11")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://github.com/rizsotto/Bear/archive/"
+                    version ".tar.gz"))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1m0w0wqnz983l7fpp5p9pdsqr7n3ybrzp8ywjcvn0rihsrzj65j6"))))
+    (build-system cmake-build-system)
+    (inputs
+     `(("python" ,python-wrapper)))
+    (arguments
+     `(#:modules ((guix build utils)
+		  (guix build cmake-build-system)
+		  ((guix build gnu-build-system) #:prefix gnu:))
+       #:phases
+       (modify-phases %standard-phases
+	 (add-before 'configure 'pss
+	   (assoc-ref gnu:%standard-phases 'patch-source-shebangs)))))
+    (home-page "https://github.com/rizsotto/Bear")
+    (synopsis "Tool for generating a compilation database")
+    (description "A JSON compilation database is used in the Clang project to
+provide information on how a given compilation unit is processed.  With this,
+it is easy to re-run the compilation with alternate programs.  Bear is used to
+generate such a compilation database.")
+    (license license:gpl3+)))
