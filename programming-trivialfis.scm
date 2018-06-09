73f3b2a36aa9ef23fb73a98a99f12252aa6772c0
fis <ybbs.daans@hotmail.com>
2018-06-09 23:01:02 +0800

    Add copying.

--- programming-trivialfis.scm
+++ programming-trivialfis.scm
@@ -1,3 +1,20 @@
+;;; Copyright © 2018 Fis Trivial <ybbs.daans@hotmail.com>
+;;;
+;;; This file is NOT part of GNU Guix.
+;;;
+;;; This file is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; This file is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with This file.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (programming-trivialfis)
   #:use-module (guix build-system trivial)
   #:use-module ((guix licenses) #:prefix license:)
