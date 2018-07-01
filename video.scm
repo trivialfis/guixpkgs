2c0c01b80f75f59db5ff9dc8cda9cd7d30d357f2
fis <ybbs.daans@hotmail.com>
2018-07-02 00:13:57 +0800

    Add libopenshot-audio.

---
+++ video.scm
@@ -0,0 +1,130 @@
+(define-module (video)
+  #:use-module (guix build-system cmake)
+  #:use-module (guix build-system python)
+  #:use-module (guix download)
+  #:use-module (guix packages)
+  #:use-module (guix build utils)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages documentation)
+  #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages imagemagick)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages networking)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages qt)
+  #:use-module (gnu packages ruby)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages swig))
+
+(define-public libopenshot-audio
+  (package
+    (name "libopenshot-audio")
+    (version "0.1.4")
+    (source
+     (origin (method url-fetch)
+             (uri
+              (string-append
+               "https://github.com/OpenShot/libopenshot-audio/archive/v"
+               version ".tar.gz"))
+             (file-name (string-append name "-" version ".tar.gz"))
+             (modules '((guix build utils)))
+             (snippet
+              '(with-directory-excursion "JuceLibraryCode/modules/juce_graphics/"
+                 (let ((files (find-files "." ".*\\.cpp|.*\\.h")))
+                   (display "find-files\n")
+                   (display files)
+                   (substitute* files
+                     (("#include ?<(ft2build\\.h)>" all header)
+                      (string-append "#include <freetype2/" header ">")))
+                   #t)))
+             (sha256
+              (base32
+               "1hd1h6r2z7brrjr41kllpvyha70209hyfnbfk04i17s8cn5g6qcy"))))
+    (arguments
+     `(#:configure-flags
+       (list (string-append "-DCMAKE_CXX_FLAGS=-I"
+                            (string-append
+                             (assoc-ref %build-inputs "freetype")
+                             "/include/freetype2")))
+       #:tests? #f))
+    (inputs
+     `(("alsa-lib" ,alsa-lib)
+       ("freetype" ,freetype)
+       ("libx11" ,libx11)
+       ("libxcursor" ,libxcursor)
+       ("libxinerama" ,libxinerama)
+       ("libxrandr" ,libxrandr)))
+    (build-system cmake-build-system)
+    (home-page "http://www.openshot.org/")
+    (synopsis "High-quality editing and playback of audio")
+    (description "program that allows the  high-quality editing and playback of
+audio, and is based on the amazing JUCE library.")
+    (license license:gpl3+)))
+
+(define-public libopenshot
+  ;; Not building yet.
+  (package
+    (name "libopenshot")
+    (version "0.1.8")
+    (source (origin
+	      (method url-fetch)
+	      (uri
+	       (string-append
+		"https://github.com/OpenShot/libopenshot/archive/v"
+		version ".tar.gz"))
+	      (sha256
+	       (base32
+		"0iax6sxky4as8iqy1d9iirz8nk5gy47krk5ir7nydh656dj71c59"))
+	      (file-name (string-append name "-" version))))
+    (build-system cmake-build-system)
+    (native-inputs
+     `(("doxygen" ,doxygen)
+       ("python" ,python-wrapper)
+       ("ruby" ,ruby)
+       ("swig" ,swig)
+       ("unittest-cpp" ,unittest-cpp)))
+    (inputs
+     `(("ffmpeg" ,ffmpeg)
+       ("imagemagick" ,imagemagick)
+       ("libopenshot-audio" ,libopenshot-audio)
+       ("qt" ,qt)
+       ("zeromq" ,zeromq)))
+    (home-page "http://www.openshot.org/")
+    (synopsis "delivering high quality video editing, animation, and playback
+solutions")
+    (description "OpenShot Library (libopenshot) is an open-source project
+dedicated to delivering high quality video editing, animation, and playback
+solutions to the world. For more information visit.")
+    (license license:lgpl3+)))
+
+(define-public openshot
+  ;; Not building yet.
+  (package
+    (name "openshot")
+    (version "2.4.2")
+    (source (origin
+	      (method url-fetch)
+	      (uri
+	       (string-append
+		"https://github.com/OpenShot/openshot-qt/archive/v"
+		version ".tar.gz"))
+	      (sha256
+	       (base32
+		"0iax6sxky4as8iqy1d9iirz8nk5gy47krk5ir7nydh656dj71c59"))
+	      (file-name (string-append name "-" version))))
+    (build-system python-build-system)
+    (native-inputs
+     `(("doxygen" ,doxygen)
+       ("swig" ,swig)
+       ("unittest-cpp" ,unittest-cpp)))
+    (propagated-inputs
+     `(("libopenshot" ,libopenshot)
+       ("python-pyqt" ,python-pyqt)))
+    (inputs
+     `(("ffmpeg" ,ffmpeg)))
+    (home-page "http://www.openshot.org/")
+    (synopsis "video editing application")
+    (description "high quality video editing and animation solutions.")
+    (license license:gpl3+)))
