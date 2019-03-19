65bf30a064aa3aa5e7819ac0f1f9a5c6c8ad975d
fis <jm.yuan@outlook.com>
2019-03-19 08:28:35 +0800

    Mark halide as wip.

--- halide.scm
+++ halide.scm
@@ -2,16 +2,17 @@
   #:use-module (guix packages)
   #:use-module (guix git-download)
   #:use-module (guix build-system cmake)
+  #:use-module (gnu packages)
   #:use-module (gnu packages llvm)
   #:use-module (llvm)
   #:use-module ((guix licenses) #:prefix license:))
 
-(define-public halide
+(define-public halide-wip
   (let* ((commit "406cd214115d6be939ecc1ef6f9f10729493189c")
 	 (revision "0")
 	 (version (string-append "2018.02.15" revision commit)))
     (package
-      (name "halide")
+      (name "halide-wip")
       (version version)
       (home-page "http://halide-lang.org/")
       (source (origin
@@ -22,11 +23,18 @@
 		(sha256
 		 (base32
 		  "0xcavm152w7yr7mdf8wizz3nclmfa15pw82mi3wby3cq9sj79aya"))
-		(file-name (git-file-name name version))))
+		(file-name (git-file-name name version))
+		(patches (search-patches "halide-Use-shared-library.patch"))))
       (build-system cmake-build-system)
       (inputs
        `(("llvm" ,llvm)
 	 ("clang" ,clang-7.0.1)))
+      (arguments
+       `(#:configure-flags
+	 (list (string-append
+		"-DCLANG_BINARY_DIR="
+		(assoc-ref %build-inputs "clang")
+		"/bin/clang"))))
       (synopsis "Embedded language for fast, portable data-parallel computation")
       (description "Halide is a programming language designed to make it easier
 to write high-performance image processing code on modern machines.")
